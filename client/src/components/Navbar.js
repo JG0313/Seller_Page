9be9bb780e3c6ac4aae40504fd51d85bf297595b
JG0313 <gonzalezj0313@gmail.com>
2024-04-04 14:29:36 -0500

    Navbar Changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -24,18 +24,18 @@ function Navbar() {
   return (
     <nav className={`navbar ${isScrolled ? 'bg' : ''}`}>
       <ul className="links-container">
-        <li className="link-item"><a href="#" className="link active">home</a></li>
-        <li className="link-item"><a href="#" className="link">product</a></li>
-        <li className="link-item"><a href="#" className="link">about</a></li>
-        <li className="link-item"><a href="#" className="link">contact</a></li>
+        <li className="link-item"><a href="#overview" className="link active">Overview</a></li>
+        <li className="link-item"><a href="#products" className="link">Products</a></li>
+        <li className="link-item"><a href="#partners" className="link">Partners</a></li>
+        <li className="link-item"><a href="#articles" className="link">Articles</a></li>
       </ul>
       <div className="user-interactions">
         <div className="cart">
-          <img src="img/cart.png" className="cart-icon" alt="" />
+          <img src={"../img/cart.png"} className="cart-icon" alt="" />
           <span className="cart-item-count">00</span>
         </div>
         <div className="user">
-          <img src="img/user.png" className="user-icon" alt="" />
+          <img src={'/img/user.png'} className="user-icon" alt="" />
         </div>
       </div>
     </nav>
