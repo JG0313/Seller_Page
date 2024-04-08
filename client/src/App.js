ddd7afeb50ce1f628905c4f21ee26e36aa778257
abreuker <98344302+abreuker@users.noreply.github.com>
2024-04-07 20:57:27 -0500

    first run at integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+
 import './assets/App.css';
+
+// Components
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import axios from 'axios';
+
+// Pages
+import Seller from "./pages/sellerLandingPage";
+import Designer from "./pages/designerLandingPage";
+import Buyer from "./pages/customerLandingPage";
+import { BuyerPageFramework, SellerPageFramework, DefaultPageFramework } from "./pages/Pageframework";
+import Structures from "./pages/structures";
+import Brands from "./pages/brands";
+import Company from "./pages/company";
+import Houses from "./pages/houses";
+import Resources from "./pages/resources";
+import Discover from "./pages/discover";
+import Design from "./pages/design";
+import Construct from "./pages/construct";
+import Community from "./pages/community";
+import Support from "./pages/support";
+import MyAccount from "./pages/dummyPages/myAccount";
+import MyFavorites from "./pages/dummyPages/myFavorites";
+import MyCart from "./pages/myCart";
+import Login from "./pages/login";
+import SampleProduct from "./pages/dummyPages/sampleProduct";
+
 import Header from './pages/Header';
 import ProductSection from './pages/ProductSection';
 import MidSection from './pages/MidSection';
 import ImageMidSection from './pages/ImageMidSection';
 import ReviewSection from './pages/ReviewSection';
 import EditPage from './pages/base-edit-overview.js';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios from 'axios';
 
 function App() {
     const [sellers, setSellers] = useState([]);
@@ -30,25 +55,73 @@ function App() {
 
     return (
         <Router>
-            <div className="app-container">
-                <Navbar />
-                <Header />
-                <div className="content-container">
-                    <ProductSection />
-                    <div className="content-container">
-                        <MidSection />                       
-                        <div className="main-content">
-                            <Routes>
-                                <Route path="/productSection" element={<ProductSection />} />
-                                <Route path="/midSection" element={<MidSection />} />
-                                <Route path="/imageMidSection" element={<ImageMidSection />} />
-                                <Route path="/reviewSection" element={<ReviewSection />} />
-                                <Route path="/edit-overview" element={<EditPage />} />
-                            </Routes>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <Routes>
+                <Route
+                    exact path="/login/seller" //Path for it to be added too
+                    element={<SellerPageFramework component={<Seller />} />} />
+                <Route
+                    exact path="/login/designer" //Path for it to be added too
+                    element={<SellerPageFramework component={<Designer />} />} />
+                <Route
+                    exact path="/login/buyer" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Buyer />} />} />
+                <Route
+                    exact path="/" //Path for it to be added too
+                    element={<DefaultPageFramework component={<Buyer />} />} />
+                <Route
+                    path="/login" //Path for it to be added too
+                    element={<DefaultPageFramework component={<Login />} />} />
+                <Route
+                    path="/houses" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Houses />} />} />
+                <Route
+                    path="/structures" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Structures />} />} />
+                <Route
+                    path="/brands" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Brands />} />} />
+                <Route
+                    path="/resources" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Resources />} />} />
+                <Route
+                    path="/company" //Path for it to be added too
+                    element={<BuyerPageFramework component={<Company />} />} />
+                <Route
+                    path="/discover" //Path for it to be added too
+                    element={<SellerPageFramework component={<Discover />} />} />
+                <Route
+                    path="/design" //Path for it to be added too
+                    element={<SellerPageFramework component={<Design />} />} />
+                <Route
+                    path="/construct" //Path for it to be added too
+                    element={<SellerPageFramework component={<Construct />} />} />
+                <Route
+                    path="/community" //Path for it to be added too
+                    element={<SellerPageFramework component={<Community />} />} />
+                <Route
+                    path="/support" //Path for it to be added too
+                    element={<SellerPageFramework component={<Support />} />} />
+                <Route
+                    path="/dummyPages/myAccount" //Path for it to be added too
+                    element={<BuyerPageFramework component={<MyAccount />} />} />
+                <Route
+                    path="/dummyPages/myFavorites" //Path for it to be added too
+                    element={<BuyerPageFramework component={<MyFavorites />} />} />
+                <Route
+                    path="/myCart" //Path for it to be added to
+                    element={<BuyerPageFramework component={<MyCart />} />} />
+                <Route
+                    path="/dummyPages/sampleProduct" //Path for it to be added too
+                    element={<DefaultPageFramework component={<SampleProduct />} />} />
+
+                <Route path="/productSection" element={<ProductSection />} />
+                <Route path="/midSection" element={<MidSection />} />
+                <Route path="/imageMidSection" element={<ImageMidSection />} />
+                <Route path="/reviewSection" element={<ReviewSection />} />
+                <Route path="/edit-overview" element={<EditPage />} />
+            </Routes>
+        </div>
             <Link to="/edit-overview" className="btn btn-primary">hello</Link>
             <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
                 <div className="w-50">
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,45 +1,155 @@
-// Navbar.js
-import React, { useState, useEffect } from 'react';
-import '../assets/Navbar.css'; 
+/// <summary>
+/// Authors: Jason Shull, Parker Libby
+/// Description: This script handles all of the links that the files need to navigate to for the navbar
+/// </summary>
 
-function Navbar() {
-  const [isScrolled, setIsScrolled] = useState(false);
+import React from "react";
+import { Nav, NavLink, NavMenu, SmallNavMenu, SmallNavLink, SearchContainer, SearchInput, SearchButton } from "./NavbarElements";
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY >= 180) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
 
-    window.addEventListener('scroll', handleScroll);
+export const DefaultNavBar = () => {
+    return (
+        <Nav>
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    <img src={require("../assets/House.png")} alt="House"></img>
+                </NavLink>
+                <NavLink to="/houses" activeStyle>
+                    Houses
+                </NavLink>
+                <NavLink to="/structures" activeStyle>
+                    Structures
+                </NavLink>
+                <NavLink to="/brands" activeStyle>
+                    Brands
+                </NavLink>
+                <NavLink to="/resources" activeStyle>
+                    Resources
+                </NavLink>
+                <NavLink to="/company" activeStyle>
+                    Company
+                </NavLink>
+            </NavMenu>
+            <SearchContainer>
+                <SearchInput type="text" placeholder="Search..." />
+                <SearchButton><img src={require("../assets/SearchIcon.png")} alt="Search" width="30" height="30" /></SearchButton>
+            </SearchContainer>
+        </Nav>
+    );
+};
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+export const BuyerNavBar = () => {
+    return (
+        <Nav>
+            <NavMenu>
+                <NavLink to="/login/buyer" activeStyle>
+                    <img src={require("../assets/House.png")} alt="House"></img>
+                </NavLink>
+                <NavLink to="/houses" activeStyle>
+                    Houses
+                </NavLink>
+                <NavLink to="/structures" activeStyle>
+                    Structures
+                </NavLink>
+                <NavLink to="/brands" activeStyle>
+                    Brands
+                </NavLink>
+                <NavLink to="/resources" activeStyle>
+                    Resources
+                </NavLink>
+                <NavLink to="/company" activeStyle>
+                    Company
+                </NavLink>
+            </NavMenu>
+            <SearchContainer>
+                <SearchInput type="text" placeholder="Search..." />
+                <SearchButton><img src={require("../assets/SearchIcon.png")} alt="Search" width="30" height="30" /></SearchButton>
+            </SearchContainer>
+        </Nav>
+    );
+};
 
-  return (
-    <nav className={`navbar ${isScrolled ? 'bg' : ''}`}>
-      <ul className="links-container">
-        <li className="link-item"><a href="#overview" className="link active">Overview</a></li>
-        <li className="link-item"><a href="#products" className="link">Products</a></li>
-        <li className="link-item"><a href="#partners" className="link">Partners</a></li>
-        <li className="link-item"><a href="#articles" className="link">Articles</a></li>
-      </ul>
-      <div className="user-interactions">
-        <div className="cart">
-          <img src={"../img/cart.png"} className="cart-icon" alt="" />
-          <span className="cart-item-count">00</span>
-        </div>
-        <div className="user">
-          <img src={'/img/user.png'} className="user-icon" alt="" />
-        </div>
-      </div>
-    </nav>
-  );
-}
+export const SellerNavBar = () => {
+    return (
+        <Nav>
+            <NavMenu>
+                <NavLink to="/login/seller" activeStyle>
+                    <img src={require("../assets/House.png")} alt="House"></img>
+                </NavLink>
+                <NavLink to="/discover" activeStyle>
+                    Discover
+                </NavLink>
+                <NavLink to="/design" activeStyle>
+                    Design
+                </NavLink>
+                <NavLink to="/construct" activeStyle>
+                    Construct
+                </NavLink>
+                <NavLink to="/community" activeStyle>
+                    Community
+                </NavLink>
+                <NavLink to="/support" activeStyle>
+                    Support
+                </NavLink>
+            </NavMenu>
+            <SearchContainer>
+                <SearchInput type="text" placeholder="Search..." />
+                <SearchButton><img src={require("../assets/SearchIcon.png")} alt="Search" width="30" height="30" /></SearchButton>
+            </SearchContainer>
+        </Nav>
+    );
+};
 
-export default Navbar;
+export const SmallerDefaultNavBar = () => {
+    return (
+        <smallNav>
+            <SmallNavMenu>
+                <SmallNavLink to="/login" activeStyle>
+                    Login
+                </SmallNavLink>
+                <SmallNavLink to="/location-editor" activeStyle>
+                    Location Editor
+                </SmallNavLink>
+                <SmallNavLink to="/message-inbox" activeStyle>
+                    Message Inbox
+                </SmallNavLink>
+            </SmallNavMenu>
+        </smallNav>
+    );
+};
+
+export const SmallerSellerNavBar = () => {
+    return (
+        <smallNav>
+            <SmallNavMenu>
+                <SmallNavLink to="/dummyPages/myaccount" activeStyle>
+                    My Account
+                </SmallNavLink>
+                <SmallNavLink to="/dummyPages/locationeditor" activeStyle>
+                    Location Editor
+                </SmallNavLink>
+                <SmallNavLink to="/dummyPages/messagesinbox" activeStyle>
+                    Message Inbox
+                </SmallNavLink>
+            </SmallNavMenu>
+        </smallNav>
+    );
+};
+
+export const SmallerBuyerNavBar = () => {
+    return (
+        <smallNav>
+            <SmallNavMenu>
+                <SmallNavLink to="/dummyPages/myaccount" activeStyle>
+                    My Account
+                </SmallNavLink>
+                <SmallNavLink to="/dummyPages/myfavorites" activeStyle>
+                    My Favorites
+                </SmallNavLink>
+                <SmallNavLink to="/mycart" activeStyle>
+                    <img src={require("../assets/Cart.png")} alt="Cart" height="30"></img>
+                </SmallNavLink>
+            </SmallNavMenu>
+        </smallNav>
+    );
+};
