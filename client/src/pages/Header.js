65f8fc9f341063bcb8c27fb6897543a4411d32aa
Taylor Sims <trsims@mail.bradley.edu>
2024-04-08 20:16:55 -0500

    Seller Page
    i'm really tired and i did a lot wrong but my team was nice and they helped me out!

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import '../assets/Header.css'; 
+import Seller from '../assets/img/seller.jpg';
+
 
 function Header() {
   return (
     <header className="header-section">
-      <h1 className="header-heading"><span>Welcome to<br /></span> CAT 3D Print Marketplace</h1>
+      <div className="header-content">
+        <img id='seller_image' src={Seller} className="seller-img" alt="Seller" />
+      </div>
+      <div className='header-card'>
+      <h1 id='seller_name' className="header-heading">
+        <span>Mark <br /></span> Johnson
+      </h1>
+      <p id='seller_website' className="header-text">www.markjohnson.org</p>
+      <p id='seller_email' className="header-text">markjohnson@gmail.com</p>
+      <p id='seller_number' className="header-text">555-555-5555</p>
+      <p id='seller_summary' className="header-text">Lorem ipsum dolor sit amet consectetur adipiscing elit.</p>
+      </div>
     </header>
   );
 }
---
+++ client/src/pages/SellerOverview.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import '../assets/App.css';
+import Navbar from '../components/SellersNavbar.js';
+import Header from '../pages/Header.js';
+import ProductSection from '../pages/ProductSection.js';
+import ImageMidSection from '../pages/ImageMidSection.js';
+import ReviewSection from '../pages/ReviewSection.js';
+import EditPage from '../pages/base-edit-overview.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function SellerOverview() {
+    return (
+        <div className="app-container">
+            <Navbar />
+            <div className="header">
+            <Header />
+            <div className="content-container">
+                <ProductSection />
+                    <div className="content-container">
+                        <ImageMidSection />
+                        <div className="content-container">
+                            <ReviewSection />                
+                        <div className="main-content">
+                            <Routes>
+                                <Route path="/productSection" element={<ProductSection />} />
+                                <Route path="/imageMidSection" element={<ImageMidSection />} />
+                                <Route path="/reviewSection" element={<ReviewSection />} />
+                                <Route path="/edit-overview" element={<EditPage />} />
+                            </Routes>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default SellerOverview;
